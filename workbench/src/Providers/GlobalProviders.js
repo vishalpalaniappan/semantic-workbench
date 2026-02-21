494ad421cf78bafc3fea3c4f194652e57e4ed34c
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-21 18:54:41 -0500

    Implement websocket connection to server with auto reconnect.

--- workbench/src/Providers/GlobalProviders.js
+++ workbench/src/Providers/GlobalProviders.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 
 import PropTypes from "prop-types";
+import useWebSocket, {ReadyState} from "react-use-websocket";
+
+import ServerContext from "./ServerContext";
 
 GlobalProviders.propTypes = {
     children: PropTypes.node,
@@ -12,10 +15,53 @@ GlobalProviders.propTypes = {
  * @return {JSX}
  */
 function GlobalProviders ({children}) {
+    // Connect and setup auto reconnect
+    const socketUrl = "ws://localhost:3002";
+    const {
+        // sendMessage,
+        sendJsonMessage,
+        lastMessage,
+        // lastJsonMessage,
+        readyState,
+        // getWebSocket,
+    } = useWebSocket(socketUrl, {
+        onOpen: () => connectionOpen(),
+        shouldReconnect: (closeEvent) => true,
+    });
+
+
+    // Called when connection is opened.
+    const connectionOpen = () => {
+        console.log("CONNECTION OPENED");
+        sendJsonMessage("I have connected");
+    };
+
+    // Sets the message history
+    const [messageHistory, setMessageHistory] = useState([]);
+    useEffect(() => {
+        if (lastMessage !== null) {
+            setMessageHistory((prev) => prev.concat(lastMessage));
+        }
+    }, [lastMessage]);
+
+
+    // Set the connection state and log to console
+    const connectionStatus = {
+        [ReadyState.CONNECTING]: "Connecting",
+        [ReadyState.OPEN]: "Open",
+        [ReadyState.CLOSING]: "Closing",
+        [ReadyState.CLOSED]: "Closed",
+        [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+    }[readyState];
+
+    useEffect(() => {
+        console.log("Websocket state:", connectionStatus);
+    }, [readyState]);
+
     return (
-        <>
+        <ServerContext.Provider value={{sendJsonMessage}}>
             {children}
-        </>
+        </ServerContext.Provider>
     );
 };
 
---
+++ workbench/src/Providers/ServerContext.js
@@ -0,0 +1,8 @@
+import {createContext} from "react";
+
+/**
+ * Context that contains the connection to the server.
+ */
+const ServerContext = createContext();
+
+export default ServerContext;
