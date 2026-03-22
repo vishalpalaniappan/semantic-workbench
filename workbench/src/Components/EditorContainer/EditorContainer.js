b8edca53752762f59aabc7713baffc1ac433bec3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-22 12:31:06 -0400

    Update layout of workbench to temporarily have two editors.

--- workbench/src/Components/EditorContainer/EditorContainer.js
+++ workbench/src/Components/EditorContainer/EditorContainer.js
@@ -1,19 +1,12 @@
-import React, {useContext, useLayoutEffect, useRef, useEffect} from "react";
+import React, {useContext, useEffect, useRef} from "react";
 
 import {Editor} from "sample-ui-component-library";
 import {useDragState} from "ui-layout-manager-dev";
 
 import ServerContext from "../../Providers/ServerContext";
+import {flattenTree} from "./helper";
 
 import "./EditorContainer.scss";
-
-EditorContainer.propTypes = {};
-
-const flattenTree = (tree, level = 0) =>
-    tree.flatMap((node) => [
-        {...node, level},
-        ...(node.children ? flattenTree(node.children, level + 1) : []),
-    ]);
 
 /**
  * Component to display the design metadata.
@@ -21,7 +14,8 @@ const flattenTree = (tree, level = 0) =>
  */
 export function EditorContainer () {
     const {workspace} = useContext(ServerContext);
-    const workspaceRef = useRef();
+    const editorRef = useRef();
+    const parentIdRef = useRef(null);
 
     const {drop} = useDragState();
 
@@ -32,27 +26,54 @@ export function EditorContainer () {
 
         const activeType = drop.activeData.type;
         const overType = drop.overData.type;
+        const activeParent = drop.activeData.parentId;
+        const overParent = drop.overData.parentId;
+
+        // Only drop files, not folders.
+        if (drop.activeData.node.type !== "file") {
+            return;
+        }
+
+        // TODO: This is a temp solution while I switch to event based arch.
         if (activeType === "EditorTab" && overType === "EditorTabGutter") {
-            workspaceRef.current.moveTab(drop.activeData.node.uid, drop.overData.index);
-        } else if (activeType === "FileTreeNode" && overType === "EditorTabGutter") {
-            workspaceRef.current.addTab(drop.activeData.node, drop.overData.index);
+            if (activeParent === overParent) {
+                // Moving within same editor
+                if (overParent === parentIdRef.current) {
+                    editorRef.current.moveTab(drop.activeData.node.uid, drop.overData.index);
+                }
+            } else {
+                // Moving between editors, need to remove the tab that was moved
+                if (overParent === parentIdRef.current) {
+                    editorRef.current.addTab(drop.activeData.node, drop.overData.index);
+                } else if (activeParent === parentIdRef.current) {
+                    editorRef.current.closeTab(drop.activeData.node.uid);
+                }
+            }
+        } else if (activeType === "FileTreeNode" && overType === "EditorTabGutter"
+            && overParent === parentIdRef.current) {
+            // Moving from fileTree to editor
+            editorRef.current.addTab(drop.activeData.node, drop.overData.index);
         }
     }, [drop]);
 
-    useLayoutEffect(() => {
-        if (workspace && workspaceRef?.current) {
-            workspaceRef.current.setTabGroupId("tab-group-1");
-            flattenTree(workspace).forEach((node) => {
-                if (node.type === "file") {
-                    workspaceRef.current.addTab(node);
-                }
-            });
+    useEffect(() => {
+        parentIdRef.current = crypto.randomUUID();
+        editorRef.current.setTabGroupId(parentIdRef.current);
+    }, []);
+
+    useEffect(() => {
+        if (workspace) {
+            // This is only for demo purposes, I am randomly loading 2 files.
+            const files = flattenTree(workspace).filter((node) => node.type === "file");
+            for (let i = 0; i < 4; i++) {
+                editorRef.current.addTab(files[Math.floor(Math.random() * 2) + 1]);
+            }
         }
     }, [workspace]);
 
     return (
         <div className="editorContainer" style={{padding: "0 3px"}}>
-            <Editor ref={workspaceRef} />
+            <Editor ref={editorRef} />
         </div>
     );
 }
