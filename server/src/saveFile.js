22fb0dbd7d30c54f81168a80b75b53af743bb828
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-25 16:05:12 -0400

    Remove unncessary comment.

--- server/src/saveFile.js
+++ server/src/saveFile.js
@@ -5,7 +5,6 @@ async function saveFile(fileName, folderPath, data) {
     const filePath = path.join(folderPath, fileName);
     try {
         await fs.writeFile(filePath, data);
-        console.log('File saved successfully');
     } catch (err) {
         console.error('Error saving file:', err);
     }
