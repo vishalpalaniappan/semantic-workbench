645675ad6315dee2c1d3f07a1972a9aed5ce6974
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-30 09:17:48 -0400

    Subscribe to status event in status bar and update UI.

--- workbench/src/Components/StatusBar/StatusBar.js
+++ workbench/src/Components/StatusBar/StatusBar.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from "react";
 
 import {CircleFill} from "react-bootstrap-icons";
+import {useLayoutEventSubscription} from "ui-layout-manager-dev";
 
 import ServerContext from "../../Providers/ServerContext";
 
@@ -17,6 +18,11 @@ export function StatusBar () {
     const {connectionStatus} = useContext(ServerContext);
 
     const [connectionColor, setConnectionColor] = useState({color: "green"});
+    const [message, setMessage] = useState("");
+
+    useLayoutEventSubscription("status:set", (event) => {
+        setMessage(event.payload);
+    });
 
     useEffect(() => {
         if (connectionStatus) {
@@ -37,6 +43,7 @@ export function StatusBar () {
     return (
         <div className="status-bar">
             <div className="status-left">
+                {message}
             </div>
             <div className="status-right">
                 <CircleFill
