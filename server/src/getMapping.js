2bc82418e41bf30f627f460b1496a77810980543
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-02 15:34:44 -0400

    Cleanup getMapping function.

--- server/src/getMapping.js
+++ server/src/getMapping.js
@@ -1,36 +1,35 @@
-import { spawn } from 'node:child_process';
+import { spawn } from "node:child_process";
 
+/**
+ * Gets the mapping for the given file by running instrumenter in stream mode.
+ * @param {String} file Path of the python script to map.
+ * @param {Array} args The arguments to pass to the instrumenter.
+ * @returns {Promise<String>} A promise that resolves with the output of the instrumenter.
+ */
 function getMapping(file, args = []) {
-  return new Promise((resolve, reject) => {
-    const process = spawn('python3', [file, ...args]);
+    return new Promise((resolve, reject) => {
+        const process = spawn("python3", ["tools/instrumenter/instrumenter.py", "--mode", "parser", file, ...args]);
 
-    let stdout = '';
-    let stderr = '';
+        let stdout = "";
+        let stderr = "";
 
-    process.stdout.on('data', (data) => {
-      stdout += data.toString();
+        process.stdout.on("data", (data) => {
+            stdout += data.toString();
+        });
+
+        process.stderr.on("data", (data) => {
+            stderr += data.toString();
+        });
+
+        process.on("close", async (code) => {
+            if (code !== 0) {
+                reject(new Error(stderr || `Process exited with code ${code}`));
+            } else {
+                const outputData = stdout.trim();
+                resolve(outputData);
+            }
+        });
     });
-
-    process.stderr.on('data', (data) => {
-      stderr += data.toString();
-    });
-
-    process.on('close', (code) => {
-      if (code !== 0) {
-        reject(new Error(stderr || `Process exited with code ${code}`));
-      } else {
-        resolve(stdout.trim());
-      }
-    });
-  });
 }
-
-getMapping('tools/instrumenter/instrumenter.py', ['--mode', 'parser', './sample.py'])
-  .then((result) => {
-    console.log('Mapping result:', result);
-  })
-  .catch((error) => {
-    console.error('Error getting mapping:', error);
-  });
 
 export default getMapping;
