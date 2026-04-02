321607a53235d39b521c777d45218d12df95684d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-02 11:25:46 -0400

    Set wordwrap to true.

--- workbench/src/Components/Modals/ShowInfo.js
+++ workbench/src/Components/Modals/ShowInfo.js
@@ -45,6 +45,7 @@ export function ShowInfo ({close, args}) {
                 options={{
                     minimap: {enabled: false},
                     lineNumbers: "off",
+                    wordWrap: "on",
                     scrollBeyondLastLine: false,
                 }}
             />
