eb81f2c1ef964668e0fbb039a8b488092ef5de58
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-04 17:01:12 -0400

    Try setting up engine as middleware.

--- workbench/src/Store/store.js
+++ workbench/src/Store/store.js
@@ -1,9 +1,21 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {DALEngine} from "dal-engine-core-js-lib-dev";
 
 import appReducer from "./appSlice";
+
+const engine = new DALEngine({
+    name: "default",
+    description: "Default engine",
+});
 
 export const store = configureStore({
     reducer: {
         app: appReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            thunk: {
+                extraArgument: {engine},
+            },
+        }),
 });
